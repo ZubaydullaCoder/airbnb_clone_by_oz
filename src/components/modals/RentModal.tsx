12b58124a936e3380653dabf420756d1065a64b2
ZubaydullaCoder <zubaydullacoder@gmail.com>
2023-11-05 17:32:58 +0500

    added image upload functionality

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -8,6 +8,10 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 
+import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
+
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -44,10 +48,18 @@ const RentModal = () => {
 
   const categorySelectedValue = watch("category");
   const locationSelectedValue = watch("location");
+  const guestCountOnChangeValue = watch("guestCount");
+  const roomCountOnChangeValue = watch("roomCount");
+  const bathroomCountOnChangeValue = watch("bathroomCount");
+  const imgSrcOnChangeUrl = watch("imageSrc");
 
-  const onBack = () => {
-    setStep((value) => value - 1);
-  };
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../global/Map"), {
+        ssr: false,
+      }),
+    [locationSelectedValue]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -57,8 +69,18 @@ const RentModal = () => {
     });
   };
 
+  const onBack = () => {
+    setStep((value) => value - 1);
+    // if (step === (STEPS.IMAGES - 1 && STEPS.IMAGES - 1)) {
+    //   setCustomValue("imageSrc", "");
+    // }
+  };
+
   const onNext = () => {
     setStep((value) => value + 1);
+    // if (step === (STEPS.IMAGES - 1 && STEPS.IMAGES - 1)) {
+    //   setCustomValue("imageSrc", "");
+    // }
   };
 
   const actionLabel = useMemo(() => {
@@ -118,9 +140,68 @@ const RentModal = () => {
           onChange={(value) => setCustomValue("location", value)}
           value={locationSelectedValue}
         />
+        <Map center={locationSelectedValue?.latlng} />
       </div>
     );
   }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCountOnChangeValue}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCountOnChangeValue}
+          onChange={(value) => setCustomValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCountOnChangeValue}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          value={imgSrcOnChangeUrl}
+          onChange={(value) => setCustomValue("imageSrc", value)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
